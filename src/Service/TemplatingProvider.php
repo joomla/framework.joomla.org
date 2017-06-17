f5d730c8e0b41e7d21b87b1a14aa403952bd2679
Michael Babker <michael.babker@gmail.com>
2017-06-16 20:59:46 -0500

    Split up the templating provider so everything is a service

--- src/Service/TemplatingProvider.php
+++ src/Service/TemplatingProvider.php
@@ -25,11 +25,13 @@ use Symfony\Component\Asset\VersionStrategy\{
 	EmptyVersionStrategy, StaticVersionStrategy
 };
 use Twig\Cache\{
-	FilesystemCache, NullCache
+	CacheInterface, FilesystemCache, NullCache
 };
 use Twig\Environment;
 use Twig\Extension\DebugExtension;
-use Twig\Loader\FilesystemLoader;
+use Twig\Loader\{
+	FilesystemLoader, LoaderInterface
+};
 use Twig\RuntimeLoader\ContainerRuntimeLoader;
 
 /**
@@ -50,39 +52,50 @@ class TemplatingProvider implements ServiceProviderInterface
 	 */
 	public function register(Container $container)
 	{
+		$container->alias(Packages::class, 'asset.packages')
+			->share('asset.packages', [$this, 'getAssetPackagesService'], true);
+
 		$container->alias(RendererInterface::class, 'renderer')
 			->alias(TwigRenderer::class, 'renderer')
 			->share('renderer', [$this, 'getRendererService'], true);
 
-		$container->share(FrameworkTwigRuntime::class, [$this, 'getFrameworkTwigRuntimeClassService'], true);
+		$container->alias(CacheInterface::class, 'twig.cache')
+			->alias(\Twig_CacheInterface::class, 'twig.cache')
+			->share('twig.cache', [$this, 'getTwigCacheService'], true);
 
-		$container->share(Packages::class, [$this, 'getPackagesClassService'], true);
+		$container->alias(Environment::class, 'twig.environment')
+			->alias(\Twig_Environment::class, 'twig.environment')
+			->share('twig.environment', [$this, 'getTwigEnvironmentService'], true);
+
+		$container->alias(DebugExtension::class, 'twig.extension.debug')
+			->alias(\Twig_Extension_Debug::class, 'twig.extension.debug')
+			->share('twig.extension.debug', [$this, 'getTwigExtensionDebugService'], true);
+
+		$container->alias(FrameworkExtension::class, 'twig.extension.framework')
+			->share('twig.extension.framework', [$this, 'getTwigExtensionFrameworkService'], true);
+
+		$container->alias(LoaderInterface::class, 'twig.loader')
+			->alias(\Twig_LoaderInterface::class, 'twig.loader')
+			->share('twig.loader', [$this, 'getTwigLoaderService'], true);
+
+		$container->alias(FrameworkTwigRuntime::class, 'twig.runtime.framework')
+			->share('twig.runtime.framework', [$this, 'getTwigRuntimeFrameworkService'], true);
+
+		$container->alias(ContainerRuntimeLoader::class, 'twig.runtime.loader')
+			->alias(\Twig_ContainerRuntimeLoader::class, 'twig.runtime.loader')
+			->share('twig.runtime.loader', [$this, 'getTwigRuntimeLoaderService'], true);
 	}
 
 	/**
-	 * Get the FrameworkTwigRuntime class service
+	 * Get the `asset.packages` service
 	 *
 	 * @param   Container  $container  The DI container.
 	 *
-	 * @return  FrameworkTwigRuntime
+	 * @return  Packages
 	 *
 	 * @since   1.0
 	 */
-	public function getFrameworkTwigRuntimeClassService(Container $container) : FrameworkTwigRuntime
-	{
-		return new FrameworkTwigRuntime($container->get(AbstractApplication::class), $container->get(Packages::class));
-	}
-
-	/**
-	 * Get the Packages class service
-	 *
-	 * @param   Container  $container  The DI container.
-	 *
-	 * @return  FrameworkTwigRuntime
-	 *
-	 * @since   1.0
-	 */
-	public function getPackagesClassService(Container $container) : Packages
+	public function getAssetPackagesService(Container $container) : Packages
 	{
 		$version = file_exists(JPATH_ROOT . '/cache/deployed.txt') ? trim(file_get_contents(JPATH_ROOT . '/cache/deployed.txt')) : md5(get_class($this));
 		$context = new ApplicationContext($container->get(AbstractApplication::class));
@@ -106,44 +119,144 @@ class TemplatingProvider implements ServiceProviderInterface
 	 */
 	public function getRendererService(Container $container) : RendererInterface
 	{
+		return new TwigRenderer($container->get('twig.environment'));
+	}
+
+	/**
+	 * Get the `twig.cache` service
+	 *
+	 * @param   Container  $container  The DI container.
+	 *
+	 * @return  \Twig_CacheInterface
+	 *
+	 * @since   1.0
+	 */
+	public function getTwigCacheService(Container $container) : \Twig_CacheInterface
+	{
 		/** @var \Joomla\Registry\Registry $config */
 		$config = $container->get('config');
 
 		// Pull down the renderer config
 		$cacheEnabled = $config->get('template.cache.enabled', false);
-		$cachePath    = $config->get('template.cache.path', '');
+		$cachePath    = $config->get('template.cache.path', 'cache/twig');
 		$debug        = $config->get('template.debug', false);
 
-		// Instantiate the Twig environment
-		$environment = new Environment(new FilesystemLoader([JPATH_TEMPLATES]), ['debug' => $debug]);
+		if ($debug === false && $cacheEnabled !== false)
+		{
+			return new FilesystemCache(JPATH_ROOT . '/' . $cachePath);
+		}
+
+		return new NullCache;
+	}
+
+	/**
+	 * Get the `twig.environment` service
+	 *
+	 * @param   Container  $container  The DI container.
+	 *
+	 * @return  Environment
+	 *
+	 * @since   1.0
+	 */
+	public function getTwigEnvironmentService(Container $container) : Environment
+	{
+		/** @var \Joomla\Registry\Registry $config */
+		$config = $container->get('config');
+
+		$debug = $config->get('template.debug', false);
+
+		$environment = new Environment(
+			$container->get('twig.loader'),
+			['debug' => $debug]
+		);
+
+		// Add the runtime loader
+		$environment->addRuntimeLoader($container->get('twig.runtime.loader'));
+
+		// Set up the environment's caching service
+		$environment->setCache($container->get('twig.cache'));
+
+		// Add the Twig extensions
+		$environment->addExtension($container->get('twig.extension.framework'));
+
+		if ($debug)
+		{
+			$environment->addExtension($container->get('twig.extension.debug'));
+		}
 
 		// Add a global tracking the debug state
 		$environment->addGlobal('fwDebug', $debug);
 
-		// Set up the environment's caching mechanism
-		$cacheService = new NullCache;
+		return $environment;
+	}
 
-		if ($debug === false && $cacheEnabled !== false)
-		{
-			$cacheService = new FilesystemCache(JPATH_ROOT . '/' . $cachePath);
-		}
+	/**
+	 * Get the `twig.extension.debug` service
+	 *
+	 * @param   Container  $container  The DI container.
+	 *
+	 * @return  DebugExtension
+	 *
+	 * @since   1.0
+	 */
+	public function getTwigExtensionDebugService(Container $container) : DebugExtension
+	{
+		return new DebugExtension;
+	}
 
-		$environment->setCache($cacheService);
+	/**
+	 * Get the `twig.extension.framework` service
+	 *
+	 * @param   Container  $container  The DI container.
+	 *
+	 * @return  FrameworkExtension
+	 *
+	 * @since   1.0
+	 */
+	public function getTwigExtensionFrameworkService(Container $container) : FrameworkExtension
+	{
+		return new FrameworkExtension;
+	}
 
-		// Add the Twig runtime loader
-		$loader = new ContainerRuntimeLoader($container);
+	/**
+	 * Get the `twig.loader` service
+	 *
+	 * @param   Container  $container  The DI container.
+	 *
+	 * @return  \Twig_LoaderInterface
+	 *
+	 * @since   1.0
+	 */
+	public function getTwigLoaderService(Container $container) : \Twig_LoaderInterface
+	{
+		return new FilesystemLoader([JPATH_TEMPLATES]);
+	}
 
-		$environment->addRuntimeLoader($loader);
+	/**
+	 * Get the `twig.runtime.framework` service
+	 *
+	 * @param   Container  $container  The DI container.
+	 *
+	 * @return  FrameworkTwigRuntime
+	 *
+	 * @since   1.0
+	 */
+	public function getTwigRuntimeFrameworkService(Container $container) : FrameworkTwigRuntime
+	{
+		return new FrameworkTwigRuntime($container->get(AbstractApplication::class), $container->get(Packages::class));
+	}
 
-		// Add our Twig extension
-		$environment->addExtension(new FrameworkExtension);
-
-		// Add the debug extension if enabled
-		if ($debug)
-		{
-			$environment->addExtension(new DebugExtension);
-		}
-
-		return new TwigRenderer($environment);
+	/**
+	 * Get the `twig.runtime.loader` service
+	 *
+	 * @param   Container  $container  The DI container.
+	 *
+	 * @return  ContainerRuntimeLoader
+	 *
+	 * @since   1.0
+	 */
+	public function getTwigRuntimeLoaderService(Container $container) : ContainerRuntimeLoader
+	{
+		return new ContainerRuntimeLoader($container);
 	}
 }
