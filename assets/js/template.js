515f3b2cc14132bbd2e10cc517630a9237268e74
Michael Babker <michael.babker@gmail.com>
2017-07-30 20:04:47 -0500

    Remove equal heights script

--- assets/js/template.js
+++ assets/js/template.js
@@ -1,31 +1,6 @@
 import smoothScroll from 'smooth-scroll';
 
 smoothScroll.init({
-    // selector: '[data-scroll]', // Selector for links (must be a class, ID, data attribute, or element tag)
-    // selectorHeader: null, // Selector for fixed headers (must be a valid CSS selector) [optional]
-    speed: 600, // Integer. How fast to complete the scroll in milliseconds
-    easing: 'easeInOutCubic', // Easing pattern to use
-    offset: 40, // Integer. How far to offset the scrolling anchor location in pixels
-    // callback: function ( anchor, toggle ) {} // Function to run after scrolling
+    speed: 600,
+    offset: 40,
 });
-
-function getHeight(elem) {
-    return Math.max(elem.scrollHeight, elem.offsetHeight, elem.clientHeight);
-}
-
-let height = 20,
-    elems = document.querySelectorAll('[data-display="equal"]');
-
-if (elems) {
-    for (let i = 0; i < elems.length; i++) {
-        let hTmp = getHeight(elems[i]);
-
-        if (hTmp > height) {
-            height = hTmp;
-        }
-    }
-
-    for (let i = 0; i < elems.length; i++) {
-        elems[i].style.height = `${height}px`;
-    }
-}
