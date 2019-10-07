6723c8994b3b3a445eb8b6fe58839526eb5d3892
Michael Babker <michael.babker@gmail.com>
2019-10-07 18:58:20 -0500

    Stop measure if event throws

--- src/Event/DebugDispatcher.php
+++ src/Event/DebugDispatcher.php
@@ -86,12 +86,17 @@ class DebugDispatcher implements DispatcherInterface
 
 		$collector->startMeasure($label);
 
-		$event = $this->dispatcher->dispatch($name, $event);
-
-		// Needed because the application's before respond event may be cut short
-		if ($collector->hasStartedMeasure($label))
+		try
 		{
-			$collector->stopMeasure($label);
+			$event = $this->dispatcher->dispatch($name, $event);
+		}
+		finally
+		{
+			// Needed because the application's before respond event may be cut short
+			if ($collector->hasStartedMeasure($label))
+			{
+				$collector->stopMeasure($label);
+			}
 		}
 
 		return $event;
