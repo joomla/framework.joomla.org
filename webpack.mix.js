d968c036c30ceb2bb86faf60e41a2d3a6ec9c551
wilsonge <georgejameswilson@googlemail.com>
2021-07-20 11:48:09 +0100

    Update to laravel mix 6

--- webpack.mix.js
+++ webpack.mix.js
@@ -7,26 +7,12 @@ mix.setPublicPath('www/media/');
 // Configure base path for media assets
 mix.setResourceRoot('/media/');
 
-// Fix for resolve-url-loader error regarding paths
-Mix.listen('configReady', (config) => {
-    for (rule of config.module.rules) {
-        if (new RegExp("\.scss$").test(rule.test.toString())) {
-            let resolveUrlLoaderIndex = 0;
-            rule.use.forEach(function (element, index) {
-                if (element.loader === "resolve-url-loader") {
-                    resolveUrlLoaderIndex = index;
-                }
-            });
-            rule.use.splice(resolveUrlLoaderIndex - 1, 0, rule.use.splice(resolveUrlLoaderIndex, 1)[0]);
-        }
-    }
-});
-
 // Core app JS
 mix.js('assets/js/template.js', 'js');
 
 // Core app CSS
 mix
+    .sourceMaps()
     .sass('assets/scss/template.scss', 'css')
     .options({
         postCss: [
